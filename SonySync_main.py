139974d1ff6b0e8dd4f9dac9d22739d6a22c854f
Anchen Sun <anchensun95@gmail.com>
2023-07-29 00:12:05 -0400

    develop main program

--- SonySync_main.py
+++ SonySync_main.py
@@ -41,49 +41,53 @@ def list_remote_folder(remote_host, remote_path):
     return folder_list
 
 def main():
-    Classroom_list = [['Debbie_School', 'StarFish']]
+    Classroom_list = [['Debbie_School', 'StarFish', ['2022']]]
 
-    for school, classroom in Classroom_list:
+    for school, classroom, year_list in Classroom_list:
         home_pth = '/nethome/anchen.sun/IBSS/{}/{}/{}_2223'.format(school, classroom, classroom)
         date_list = list_remote_folder('pegasus.ccs.miami.edu', home_pth)
-        for date in date_list:
-            date_pth = '{}/{}'.format(home_pth, date)
-            data_folder = list_remote_folder('pegasus.ccs.miami.edu', date_pth)
-            subfolder_flag = 0
-            if 'Spencer_Data' in data_folder:
-                data_folder_name = 'Spencer_Data'
-                subfolder_flag = 1
-            elif 'SPENCER_DATA' in data_folder:
-                data_folder_name = 'SPENCER_DATA'
-                subfolder_flag = 1
-            elif 'SONY_Data' in data_folder:
-                data_folder_name = 'SONY_Data'
-            elif 'LENA_Data' in data_folder:
-                data_folder_name = 'LENA_Data'
-            else:
-                print('Error: Not able to detect Data in {}'.format(date_pth))
-                break
-
-            audio_pth = '{}/{}'.format(date_pth, data_folder_name)
-            if subfolder_flag == 1:
-                folder_get = list_remote_folder('pegasus.ccs.miami.edu', audio_pth)
-                if len(folder_get) == 1:
-                    audio_pth = '{}/{}'.format(audio_pth, folder_get[0])
-                    folder_get = list_remote_folder('pegasus.ccs.miami.edu', audio_pth)
-                    if 'Audio' in folder_get:
-                        audio_pth = '{}/Audio'.format(audio_pth)
+        for year in year_list:
+            for date in date_list:
+                if date.split('-')[-1] == year:
+                    date_pth = '{}/{}'.format(home_pth, date)
+                    data_folder = list_remote_folder('pegasus.ccs.miami.edu', date_pth)
+                    subfolder_flag = 0
+                    if 'Spencer_Data' in data_folder:
+                        data_folder_name = 'Spencer_Data'
+                        subfolder_flag = 1
+                    elif 'SPENCER_DATA' in data_folder:
+                        data_folder_name = 'SPENCER_DATA'
+                        subfolder_flag = 1
+                    elif 'SONY_Data' in data_folder:
+                        data_folder_name = 'SONY_Data'
+                    elif 'LENA_Data' in data_folder:
+                        data_folder_name = 'LENA_Data'
                     else:
-                        print('Error: Not able to detect audio path in {}'.format(audio_pth))
+                        print('Error: Not able to detect Data in {}'.format(date_pth))
                         break
-                else:
-                    print('Error: Not able to detect audio path in {}'.format(audio_pth))
-                    break
-            date_file_dic = '{}_{}'.format(classroom, date)
-            command = 'scp -r anchen.sun@pegasus.ccs.miami.edu:{} {}'.format(audio_pth, date_file_dic)
-            os.system(command)
-            command = 'python test_flag_class.py --input_path {} --output_path {}'.format(date_file_dic, date_file_dic)
-            os.system(command)
-            print('Successfully generate {}'.format(date_file_dic))
+
+                    audio_pth = '{}/{}'.format(date_pth, data_folder_name)
+                    if subfolder_flag == 1:
+                        folder_get = list_remote_folder('pegasus.ccs.miami.edu', audio_pth)
+                        if len(folder_get) == 1:
+                            audio_pth = '{}/{}'.format(audio_pth, folder_get[0])
+                            folder_get = list_remote_folder('pegasus.ccs.miami.edu', audio_pth)
+                            if 'Audio' in folder_get:
+                                audio_pth = '{}/Audio'.format(audio_pth)
+                            else:
+                                print('Error: Not able to detect audio path in {}'.format(audio_pth))
+                                break
+                        elif 'SONY_Data' in folder_get:
+                            audio_pth = '{}/SONY_Data'.format(audio_pth)
+                        else:
+                            print('Error: Not able to detect audio path in {}'.format(audio_pth))
+                            break
+                    date_file_dic = '{}_{}'.format(classroom, date)
+                    command = 'scp -r anchen.sun@pegasus.ccs.miami.edu:{} {}'.format(audio_pth, date_file_dic)
+                    os.system(command)
+                    command = 'python test_flag_class.py --input_path {} --output_path {}'.format(date_file_dic, date_file_dic)
+                    os.system(command)
+                    print('Successfully generate {}'.format(date_file_dic))
 
 
 if __name__ == '__main__':
