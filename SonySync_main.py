125788c8a3a290219827851dfcd42e4ffeadfacf
Anchen Sun <anchensun95@gmail.com>
2023-08-02 00:14:50 -0400

    develop main program

--- SonySync_main.py
+++ SonySync_main.py
@@ -41,7 +41,7 @@ def list_remote_folder(remote_host, remote_path):
     return folder_list
 
 def main():
-    Classroom_list = [['Debbie_School', 'StarFish', ['2022']]]
+    Classroom_list = [['Debbie_School', 'LadyBugs', ['2022', '2023']]]
 
     for school, classroom, year_list in Classroom_list:
         home_pth = '/nethome/anchen.sun/IBSS/{}/{}/{}_2223'.format(school, classroom, classroom)
